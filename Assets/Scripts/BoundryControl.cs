c4450f2493c52ad910cd34469b315e8c22beff7d
filip <filipandalex17@gmail.com>
2020-06-08 03:40:26 +0300

    Made the following :\n-Player movement\n-Bounding box\n-Asteroids

---
+++ Assets/Scripts/BoundryControl.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundryControl : MonoBehaviour
+{
+    void OnTriggerExit(Collider other){
+        Destroy(other.gameObject);
+        
+    }
+}
---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.tag != "Boundry")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+    }
+}
